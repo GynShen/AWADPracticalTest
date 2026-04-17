645f0be559ae49f1132e0eae4937244aeafc8f61
GynShen <gynshen@gmail.com>
2026-04-17 18:31:28 +0800

    Seeder

---
+++ database/seeders/Student2205781Seeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class Student2205781Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('participants')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10) . '@example.com',
+                'password' => $i,
+            ]);
+        }
+    }
+}
