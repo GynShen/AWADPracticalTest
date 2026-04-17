a59f950f0ead25e17a3b46f083e852be4191c30d
GynShen <gynshen@gmail.com>
2026-04-17 18:42:52 +0800

    Proper Way for Seeder

--- database/seeders/Student2205781Seeder.php
+++ database/seeders/Student2205781Seeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\Participant;
 
 class Student2205781Seeder extends Seeder
 {
@@ -16,7 +17,7 @@ class Student2205781Seeder extends Seeder
     public function run()
     {
         for ($i = 0; $i < 10; $i++) {
-            DB::table('participants')->insert([
+            Participant::create([
                 'name' => Str::random(10),
                 'email' => Str::random(10) . '@example.com',
                 'password' => $i,
