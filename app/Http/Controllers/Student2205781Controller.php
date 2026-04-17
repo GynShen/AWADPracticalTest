5ffa5bb18dc362c291407f0dad734c497c217546
GynShen <gynshen@gmail.com>
2026-04-17 18:39:14 +0800

    Participant Controller

---
+++ app/Http/Controllers/Student2205781Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Participant;
+
+class Student2205781Controller extends Controller
+{
+    public function addUser(Request $req){
+        $data = $req->all();
+        Participant::create($data);
+        return ("sign up successful");
+    }
+}
